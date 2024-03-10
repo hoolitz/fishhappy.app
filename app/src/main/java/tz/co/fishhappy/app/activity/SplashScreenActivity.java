60c66a285c406fa8f48b0d731948dbf7e55bd1c6
alex yombo <lilkevinrobert@gmail.com>
2024-03-10 15:38:05 +0300

    Device token Service

--- app/src/main/java/tz/co/fishhappy/app/activity/SplashScreenActivity.java
+++ app/src/main/java/tz/co/fishhappy/app/activity/SplashScreenActivity.java
@@ -3,9 +3,16 @@ package tz.co.fishhappy.app.activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 import tz.co.fishhappy.app.R;
 
@@ -40,6 +47,24 @@ public class SplashScreenActivity extends AppCompatActivity {
             startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
             finish();
         }
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.d("ACCESS_TOKEN", "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+
+                        Log.d("ACCESS_TOKEN", "Token: "+token);
+                    }
+                });
 
     }
 }
--- app/src/main/java/tz/co/fishhappy/app/firebase/MyFirebaseMessagingService.java
+++ app/src/main/java/tz/co/fishhappy/app/firebase/MyFirebaseMessagingService.java
@@ -51,7 +51,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 PendingIntent.FLAG_ONE_SHOT);
 
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this,"notify")
                 .setSmallIcon(R.drawable.ic_stat_notification)
                 .setContentTitle(title)
                 .setContentText(messageBody)
